a5c769dc9d271068b5c9b0b535bb60fc6e4f1ff6
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2026-05-26 22:02:03 -0700

    Update logs to use pulse::Log

--- vigil/get_account_handler.cc
+++ vigil/get_account_handler.cc
@@ -1,7 +1,7 @@
-#include <iostream>
 #include <memory>
 #include <string>
 
+#include "pulse/core/log.h"
 #include "pulse/core/result.h"
 #include "pulse/core/stringify.h"
 #include "pulse/http/handler.h"
@@ -22,30 +22,25 @@ class GetAccountHandler final : public pulse::http::Handler {
       const pulse::http::Request& request) const override {
     auto name = request.path_params.find("name");
     if (name == request.path_params.end()) {
-      std::cerr << "[" << __FILE__ << ":" << __LINE__
-                << "] GetAccount: missing parameter 'name'\n";
+      pulse::Log() << "GetAccount: missing parameter 'name'";
       return pulse::http::Response{.content_type = "application/json",
                                    .status = 400,
                                    .body = R"({"status": "invalid argument"})"};
     }
 
-    std::cerr << "[" << __FILE__ << ":" << __LINE__
-              << "] GetAccount: handling request (name='" << name->second
-              << "')\n";
+    pulse::Log() << "GetAccount: handling request (name='" << name->second
+                 << "')";
 
     pulse::Result<Account> account = dao.GetAccount(name->second);
     if (!account.ok()) {
-      std::cerr << "[" << __FILE__ << ":" << __LINE__
-                << "] GetAccount: lookup failed: " << account.error().message
-                << "\n";
+      pulse::Log() << "GetAccount: lookup failed: " << account.error().message;
       return pulse::http::Response{.content_type = "application/json",
                                    .status = 404,
                                    .body = R"({"status": "not found"})"};
     }
 
-    std::cerr << "[" << __FILE__ << ":" << __LINE__
-              << "] GetAccount: lookup succeeded: "
-              << pulse::to_string(*account) << "\n";
+    pulse::Log() << "GetAccount: lookup succeeded: "
+                 << pulse::to_string(*account);
 
     return pulse::http::Response{
         .content_type = "application/json",
--- vigil/insert_account_handler.cc
+++ vigil/insert_account_handler.cc
@@ -1,6 +1,6 @@
-#include <iostream>
 #include <memory>
 
+#include "pulse/core/log.h"
 #include "pulse/core/result.h"
 #include "pulse/http/handler.h"
 #include "pulse/http/request.h"
@@ -20,8 +20,7 @@ class InsertAccountHandler final : public pulse::http::Handler {
       const pulse::http::Request& request) const override {
     auto name = request.query_params.find("name");
     if (name == request.query_params.end()) {
-      std::cerr << "[" << __FILE__ << ":" << __LINE__
-                << "] InsertAccount: missing parameter 'name'\n";
+      pulse::Log() << "InsertAccount: missing parameter 'name'";
       return pulse::http::Response{.content_type = "application/json",
                                    .status = 400,
                                    .body = R"({"status": "invalid argument"})"};
@@ -29,8 +28,7 @@ class InsertAccountHandler final : public pulse::http::Handler {
 
     auto type = request.query_params.find("type");
     if (type == request.query_params.end()) {
-      std::cerr << "[" << __FILE__ << ":" << __LINE__
-                << "] InsertAccount: missing parameter 'type'\n";
+      pulse::Log() << "InsertAccount: missing parameter 'type'\n";
       return pulse::http::Response{.content_type = "application/json",
                                    .status = 400,
                                    .body = R"({"status": "invalid argument"})"};
@@ -38,32 +36,28 @@ class InsertAccountHandler final : public pulse::http::Handler {
 
     Account::Type account_type = to_account_type(type->second);
     if (account_type == Account::Type::kUnknown) {
-      std::cerr << "[" << __FILE__ << ":" << __LINE__
-                << "] InsertAccount: unrecognized type '" << type->second
-                << "'\n";
+      pulse::Log() << "InsertAccount: unrecognized type '" << type->second
+                   << "'";
       return pulse::http::Response{.content_type = "application/json",
                                    .status = 400,
                                    .body = R"({"status": "invalid argument"})"};
     }
 
-    std::cerr << "[" << __FILE__ << ":" << __LINE__
-              << "] InsertAccount: handling request (name='" << name->second
-              << "', type='" << type->second << "')\n";
+    pulse::Log() << "InsertAccount: handling request (name='" << name->second
+                 << "', type='" << type->second << "')";
 
     if (pulse::Result<void> account =
             dao.InsertAccount(name->second, account_type);
         !account.ok()) {
-      std::cerr << "[" << __FILE__ << ":" << __LINE__
-                << "] InsertAccount: insert failed: " << account.error().message
-                << "\n";
+      pulse::Log() << "] InsertAccount: insert failed: "
+                   << account.error().message;
       return pulse::http::Response{.content_type = "application/json",
                                    .status = 500,
                                    .body = R"({"status": "internal"})"};
     }
 
-    std::cerr << "[" << __FILE__ << ":" << __LINE__
-              << "] InsertAccount: insert succeeded (name='" << name->second
-              << "')\n";
+    pulse::Log() << "InsertAccount: insert succeeded (name='" << name->second
+                 << "')";
 
     return pulse::http::Response{
         .content_type = "text/plain", .status = 201, .body = "Created"};
